9d270a6446a4e4774c487b74565a65e45fab40af
Carlos <carlos.flach15@gmail.com>
2024-09-04 18:45:31 -0300

    feat: creates route, controller and view to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+
+    # works
+    # redirect_to article_path(@article.id)
+
+    # also works
+    # redirect_to article_path(@article)
+
+    # also works - shortcut
+    redirect_to @article
+  end
 end
